47e0b114f65c5fc6c4e14e346d5ded86170d5f92
Victor Magalhães <aheia1412@gmail.com>
2017-06-07 23:27:28 -0700

    Initial

---
+++ PokemonCareTaker/Pokemon.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace PokemonCareTaker
+{
+	class Pokemon
+	{
+		// List of attributes
+		private int m_iNumber;
+		private string m_sName;
+		private string m_sType;
+		private int m_iLevel;
+		private int m_iHP;
+		private int m_iHunger;
+		private int m_iSleep;
+		private int m_iMaxAtributeValue;
+		private int m_iLoyalty;
+		private int m_iExperiencePoints;
+
+		// Getters and Setters
+		public int INumber { get => m_iNumber; set => m_iNumber = value; }
+		public string SName { get => m_sName; set => m_sName = value; }
+		public string SType { get => m_sType; set => m_sType = value; }
+		public int ILevel { get => m_iLevel; set => m_iLevel = value; }
+		public int IHP { get => m_iHP; set => m_iHP = value; }
+		public int IHunger { get => m_iHunger; set => m_iHunger = value; }
+		public int ISleep { get => m_iSleep; set => m_iSleep = value; }
+		public int IMaxAtributeValue { get => m_iMaxAtributeValue; set => m_iMaxAtributeValue = value; }
+		public int ILoyalty { get => m_iLoyalty; set => m_iLoyalty = value; }
+		public int IExperiencePoints { get => m_iExperiencePoints; set => m_iExperiencePoints = value; }
+
+		// Constructor from other Pokemon
+		public Pokemon(Pokemon pPokemonToCopy)
+		{
+			this.INumber = pPokemonToCopy.INumber;
+			this.SName = pPokemonToCopy.SName;
+			this.SType = pPokemonToCopy.SType;
+			this.ILevel = 5;
+			this.IHP = 15;
+			this.IHunger = 15;
+			this.ISleep = 15;
+			this.ILoyalty = 15;
+			this.IMaxAtributeValue = 15;
+			this.IExperiencePoints = 0;
+		}
+
+		// Constructor by attributes
+		public Pokemon(int iNumber, string sName, string sType)
+		{
+			this.INumber = iNumber;
+			this.SName = sName;
+			this.SType = sType;
+			this.ILevel = 5;
+			this.IHP = 15;
+			this.IHunger = 15;
+			this.ISleep = 15;
+			this.ILoyalty = 15;
+			this.IMaxAtributeValue = 15;
+			this.IExperiencePoints = 0;
+		}
+
+		// Method to return if Pokemon is ok to continue game or not
+		public bool IsWell()
+		{
+			return this.IHP > 0 && this.ISleep > 0 && this.IHunger > 0 && this.ILoyalty > 0;
+		}
+
+		// Method for Leveling Up: It will increase the level, some attributes and set experience to zero again
+		public void LevelUp()
+		{
+			this.ILevel++;
+			this.IMaxAtributeValue += this.ILevel;
+			this.ILoyalty += this.ILevel;
+			this.IHunger += this.ILevel;
+			this.ISleep += this.ILevel;
+			this.IExperiencePoints = 0;
+		}
+
+		// Method to Feed Pokemon: It increase hunger and HP, but decrease sleep
+		public void Feed()
+		{
+			this.IHunger = SumAndCheckAttributes(this.IHunger, 2);
+			this.IHP = SumAndCheckAttributes(this.IHP, 1);
+			this.ISleep -= 1;
+
+			this.Answer($"You fed { this.SName }. It loved your food!");
+		}
+
+		// Method to put pokemon to sleep: It restores the Pokemon's HP and sleepness, but make it hunger and less loyalty
+		public void Sleep()
+		{
+			this.ISleep = SumAndCheckAttributes(this.ISleep, 2);
+			this.IHP = SumAndCheckAttributes(this.IHP, 1);
+			this.IHunger -= 2;
+			this.ILoyalty -= 1;
+
+			this.Answer($"You put { this.SName } to sleep. It's now resting!");
+		}
+
+		// Method to put pokemon to practice: It increases the experience in battle and also the loyalty, but take some HP. In this method we also check to level up
+		public void Practice()
+		{
+			this.IExperiencePoints += this.ILevel;
+			this.ILoyalty = SumAndCheckAttributes(this.ILoyalty, 1);
+			this.IHP -= 2;
+
+			if (this.IExperiencePoints % (this.ILevel * 10) == 0)
+			{
+				this.LevelUp();
+				this.Answer($"You trained { this.SName }. So much training make it stronger! It raised one level!");
+			}
+			else
+			{
+				this.Answer($"You trained { this.SName }. Keep training and it could level up!");
+			}
+		}
+
+		// Get feedback by stats
+		public void Feedback()
+		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+			// Check for HP
+			if (this.IHP <= 5)
+			{
+				Console.WriteLine($"{ this.SName } is dying do something!");
+			}
+			else if (this.IHP > 5 && this.IHP < 10)
+			{
+				Console.WriteLine($"Be caureful with { this.SName }'s health.");
+			}
+			else
+			{
+				Console.WriteLine($"{ this.SName } is full of life.");
+			}
+
+			// Check for Sleep
+			if (this.ISleep <= 5)
+			{
+				Console.WriteLine($"{ this.SName } is dying because it's so tired!");
+			}
+			else if (this.ISleep > 5 && this.ISleep < 10)
+			{
+				Console.WriteLine($"{ this.SName } need to rest.");
+			}
+			else
+			{
+				Console.WriteLine($"{ this.SName } is full of energy.");
+			}
+
+			// Check for Hunger
+			if (this.IHunger <= 5)
+			{
+				Console.WriteLine($"{ this.SName } is too hungry! Do something!");
+			}
+			else if (this.IHunger > 5 && this.IHunger < 10)
+			{
+				Console.WriteLine($"{ this.SName } need to eat.");
+			}
+			else
+			{
+				Console.WriteLine($"{ this.SName } is full of food, almost rolling.");
+			}
+
+			// Check for Loyalty
+			if (this.ILoyalty <= 5)
+			{
+				Console.WriteLine($"{ this.SName } is one step to not obey you anymore!");
+			}
+			else if (this.ILoyalty > 5 && this.ILoyalty < 10)
+			{
+				Console.WriteLine($"{ this.SName } is becoming a rebel. Be caureful.");
+			}
+			else
+			{
+				Console.WriteLine($"{ this.SName } love to do everything you ask.");
+			}
+
+			Console.ResetColor();
+			Console.ReadKey();
+		}
+
+		// Method to pet pokemon: Creates a boundary with the pokemon. Increases loyalty, but make it more sleepy and hunger.
+		public void Pet()
+		{
+			this.ILoyalty = SumAndCheckAttributes(this.ILoyalty, 2);
+			this.ISleep -= 2;
+			this.IHunger -= 1;
+
+			this.Answer($"You pet { this.SName }. Creating affection is very good for both of you!");
+		}
+
+		// Method to put pokemon inside the pokeball: It restores the Pokemon's HP, but reduces loyalty and make it sleepy.
+		public void PutInsideThePokeball()
+		{
+			this.IHP = SumAndCheckAttributes(this.IHP, 2);
+			this.ILoyalty -= 2;
+			this.ISleep -= 1;
+
+			this.Answer($"You put { this.SName } inside the pokeball. It is a good place to rest.");
+		}
+
+		// Auxiliar method to control the max attribute value. That is, any attribute can only reach the max value.
+		private int SumAndCheckAttributes(int iAttribute, int iToAdd)
+		{
+			iAttribute += iToAdd;
+			if (iAttribute > this.IMaxAtributeValue)
+			{
+				return this.IMaxAtributeValue;
+			}
+
+			return iAttribute;
+		}
+
+		// Restore the Pokemon's stats to begginig.
+		public void RevivePokemon()
+		{
+			this.ILevel = 5;
+			this.IHP = 15;
+			this.IHunger = 15;
+			this.ISleep = 15;
+			this.ILoyalty = 15;
+			this.IMaxAtributeValue = 15;
+			this.IExperiencePoints = 0;
+		}
+
+		// Friendly way to show pokemon's answer to user
+		private void Answer(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine(message);
+			Console.ResetColor();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ PokemonCareTaker/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokemonCareTaker
+{
+	class Program
+	{
+		static void Main()
+		{
+			// Create a list of available pokemons to choose in the beggining of the game
+			List<Pokemon> pAvailablePokemons = new List<Pokemon>();
+
+			// Add to the list pokemon through constructor
+			pAvailablePokemons.Add(new Pokemon(
+				iNumber: 1,
+				sName: "Bulbasaur",
+				sType: "Leaf"
+			));
+
+			// Add to the list pokemon through constructor
+			pAvailablePokemons.Add(new Pokemon(
+				iNumber: 2,
+				sName: "Squirtle",
+				sType: "Water"
+			));
+
+			// Add to the list pokemon through constructor
+			pAvailablePokemons.Add(new Pokemon(
+				iNumber: 3,
+				sName: "Charmander",
+				sType: "Fire"
+			));
+
+			// UI to get the Pokemon
+			Pokemon pChosenPokemon = ChoosingPokemonToTakeCare(pAvailablePokemons);
+
+			// GameLoop
+			GameLoop(pChosenPokemon);
+
+		}
+
+		private static void GameLoop(Pokemon pChosenPokemon)
+		{
+			// Set initial control variables to the game.
+			int iDaysCounter = 1;
+			int iActionsCounter = 0;
+			int iMaxActionsPerDay = 2;
+			bool bStillPlaying = true;
+
+			// The major game loop and interaction to user is provided to this for. It checks if user is still playing and keep showing the stats.
+			for (int i = 0; bStillPlaying; i++)
+			{
+				// Control variables inside every iteraction.
+				string sTimeOfTheDay = iActionsCounter == 0 ? "Morning" : "Night";
+				List<string> sActions = new List<string>() { "Put to Sleep", "Feed", "Train", "Pet", "Put inside the Pokeball" };
+
+				// UI to show Pokemon Stats
+				Console.Clear();
+				Console.WriteLine("=====================");
+				Console.WriteLine($"Day: { iDaysCounter } - { sTimeOfTheDay }");
+				Console.WriteLine("=====================");
+				Console.WriteLine($"{ pChosenPokemon.SName } - Lvl. { pChosenPokemon.ILevel }");
+				Console.WriteLine("=====================");
+				Console.WriteLine($"HP: { pChosenPokemon.IHP } / { pChosenPokemon.IMaxAtributeValue }");
+				Console.WriteLine($"Hunger: { pChosenPokemon.IHunger } / { pChosenPokemon.IMaxAtributeValue }");
+				Console.WriteLine($"Sleep: { pChosenPokemon.ISleep } / { pChosenPokemon.IMaxAtributeValue }");
+				Console.WriteLine($"Loyalty: { pChosenPokemon.ILoyalty } / { pChosenPokemon.IMaxAtributeValue }");
+				Console.WriteLine($"Experience Points: { pChosenPokemon.IExperiencePoints }");
+				Console.WriteLine("=====================\n");
+				Console.WriteLine($"What would you like to do with { pChosenPokemon.SName }? [Pick by the number]");
+
+				// Show available actions in kind of a menu
+				for (int j = 0; j < sActions.Count; j++)
+				{
+					Console.WriteLine($"#{ j + 1 } { sActions[j] }");
+				}
+				Console.WriteLine("=====================");
+
+				// Get proper user input to execute actions
+				while (true)
+				{
+					// Getting input and parse to int
+					string sUserInput = Console.ReadLine();
+					bool bIsActionValid = int.TryParse(sUserInput, out int iInputInteger);
+
+					// If is a valid action...
+					if (bIsActionValid && iInputInteger >= 1 && iInputInteger <= sActions.Count)
+					{
+						// the execute it
+						ExecutePokemonAction(iInputInteger, pChosenPokemon);
+
+						// and increase the actions counter as well
+						iActionsCounter++;
+
+						// and also if it reached the maximum available, then...
+						if (iActionsCounter == iMaxActionsPerDay)
+						{
+							// Set actions counter to zero and start a new day.
+							iActionsCounter = 0;
+							iDaysCounter++;
+
+							// and get feedback of that day.
+							pChosenPokemon.Feedback();
+						}
+						break;
+					}
+					else
+					{
+						// UI to call user to repeat input
+						PrintErrorMessage($"{ pChosenPokemon.SName } don't understand your command. Try again.");
+					}
+				}
+
+				// Check if the Pokemon is in conditions to keep playing
+				if (!pChosenPokemon.IsWell())
+				{
+					// If not, then game over... UI for Game Over
+					Console.WriteLine("Prof. Oak: Ok, my assistant. Accidents happen!");
+					Console.WriteLine("Prof. Oak: Fortunately we have Nurse Joy to help us.");
+
+					while (true)
+					{
+						// Option to keep playing and user input
+						Console.WriteLine($"Prof. Oak: Would you like to keep taking care of { pChosenPokemon.SName }? [Y,n]");
+						string sUserInputKeepPlaying = Console.ReadLine();
+
+						// If keep playing...
+						if (sUserInputKeepPlaying.Equals("Y", StringComparison.OrdinalIgnoreCase))
+						{
+							// Back to first day, set actions to zero and revive Pokemon.
+							iActionsCounter = 0;
+							iDaysCounter = 1;
+							pChosenPokemon.RevivePokemon();
+							break;
+						}
+						else if (sUserInputKeepPlaying.Equals("N", StringComparison.OrdinalIgnoreCase))
+						{
+							// Break the loop and finish the game
+							Console.WriteLine("Prof. Oak: Hmm... Guess I was wrong about you, but it's okay! Have a nice day!");
+							bStillPlaying = false;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		private static void ExecutePokemonAction(int iInputInteger, Pokemon pChosenPokemon)
+		{
+			// Call the Pokemon's class method by number in actions menu
+			switch (iInputInteger)
+			{
+				case 1:
+					pChosenPokemon.Sleep();
+					break;
+				case 2:
+					pChosenPokemon.Feed();
+					break;
+				case 3:
+					pChosenPokemon.Practice();
+					break;
+				case 4:
+					pChosenPokemon.Pet();
+					break;
+				case 5:
+					pChosenPokemon.PutInsideThePokeball();
+					break;
+			}
+		}
+
+		private static Pokemon ChoosingPokemonToTakeCare(List<Pokemon> pAvailablePokemons)
+		{
+			// Context of the game.
+			Console.WriteLine("Pokemon Caretaker 101");
+			Console.WriteLine("=====================");
+
+			Console.WriteLine("\nHello! My name's Prof. Oak!");
+			Console.WriteLine($"I heard that you'd like to become my assistant...");
+			Console.WriteLine("The job isn't easy, but I think you fit the role.");
+
+			Console.WriteLine($"\nAs long as you are new here I will give you only one Pokemon to take.");
+			Console.ReadKey();
+
+			Console.Clear();
+			Console.WriteLine("Ok! No more small talk! We have a lot of things to do!");
+			Console.WriteLine("\nYou can choose to take care between...\n");
+			// End of context
+
+			// Show available options for the pokemons
+			foreach (var pPokemon in pAvailablePokemons)
+			{
+				Console.WriteLine($"#{ pPokemon.INumber } { pPokemon.SName } - Type: { pPokemon.SType }");
+			}
+
+			Pokemon pChosenPokemon;
+			while (true)
+			{
+				// Get the pokemon by name or number
+				Console.WriteLine("\nWhich one will be under your responsability? [Choose by number or name]");
+				string sUserInput = Console.ReadLine();
+				bool bIsThePokemonNumber = int.TryParse(sUserInput, out int iInputInteger);
+
+				// try to get the pokemon in the pokemon's list... if success, then break this loop.
+				try
+				{
+					pChosenPokemon = bIsThePokemonNumber ? new Pokemon(pAvailablePokemons[iInputInteger - 1]) : new Pokemon(pAvailablePokemons[pAvailablePokemons.FindIndex(p => p.SName.ToUpper() == sUserInput.ToUpper())]);
+					break;
+				}
+				catch (Exception)
+				{
+					// If not, then call user to try again
+					PrintErrorMessage("You know, I'm starting to become deaf! Could you try again?");
+				}
+			}
+			return pChosenPokemon;
+		}
+
+		// Auxiliar function to print friendly error messages
+		private static void PrintErrorMessage(string sMessage)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(sMessage);
+			Console.ResetColor();
+		}
+	}
+}
